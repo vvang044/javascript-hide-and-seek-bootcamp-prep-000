288e81dc1990cd13cc8c4d52d351eccbc52de508
Vik Vangala <vvang044@gmail.com>
2019-06-06 15:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for( var i=0; i<ranks.length;i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
