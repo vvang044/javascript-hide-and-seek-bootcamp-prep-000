c23d8763174e2c47ff78228cc97d0260889e9685
Vik Vangala <vvang044@gmail.com>
2019-06-06 15:50:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for( var i=0; i<ranks.length;i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
